5887c28f104c892f280c2d470473fde3395f7631
0xSamuelSukarno <sukarnosamuel1@gmail.com>
2022-12-08 07:21:37 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-2709224d467bd600.js
+++ _next/static/chunks/webpack-2709224d467bd600.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "7cc0de504e4aa7ca" + ".css";
+/******/ 			return "static/css/" + "13cac7db00a65aca" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
